9bc4a49bd4f77df065077fb4aed1494177bb9fef
arunkumar-source <arunkumar@webzenith.tech>
2026-02-04 02:52:53 +0530

    main

--- apps/server/tsup.config.ts
+++ apps/server/tsup.config.ts
@@ -3,10 +3,11 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm"],
+  outDir: "api",
   platform: "node",
   target: "node18",
-  bundle: true,
-  external: ["@repo/db", "@repo/shared"],
+  noExternal: ["fs", "dotenv", "@repo/db", "@repo/shared"],
+  dts: false,
   clean: true,
   sourcemap: false,
 });
