afba2a051d06d2c015e82c949538fff1ca271fad
arunkumar-source <arunkumar@webzenith.tech>
2026-02-04 00:06:38 +0530

    fix database env variable

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -4,8 +4,8 @@ import { drizzle } from "drizzle-orm/postgres-js"
 
 dotenv.config()
 
-console.log("DATABASE_URL:", process.env.DATABASE_URL ? "Set" : "Not set")
+console.log("DATABASE_URL:", process.env.DB_URL ? "Set" : "Not set")
 
-const client = postgres(process.env.DATABASE_URL!)
+const client = postgres(process.env.DB_URL!)
 
 export const db= drizzle(client)
