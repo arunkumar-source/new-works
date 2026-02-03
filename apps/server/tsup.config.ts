6f4f184e9e23083e4cb743911eb9b5e9ba0a414b
arunkumar-source <arunkumar@webzenith.tech>
2026-02-03 21:42:19 +0530

    main 2

--- apps/server/tsup.config.ts
+++ apps/server/tsup.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['./src/index.ts'],
+  entry: ['./src/index.(ts|js)'],
   splitting: false,
   sourcemap: false,
   clean: false,
   noExternal:["@repo/db","@repo/shared"],
   format:"esm",
-  outDir:"."
+  outDir:"./src"
 })
